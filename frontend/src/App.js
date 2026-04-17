41bb8d79ba5aeaf100b0c8aeef4c42182aa41fa1
Asolo <delightsomedunsiasolo24@gmail.com>
2026-04-17 01:43:25 +0100

    2 to to

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
 					<div key={p.id} className="card">
 						<h3>{p.name}</h3>
 						<p>${p.price}</p>
-						<button>Add 2 Cart</button>
+						<button>Add to Cart</button>
 					</div>
 				))}
 			</div>
